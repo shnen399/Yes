5e513b12d7d341008edb8ba636c9c6d120358337
shnen399 <t0970313177@gmail.com>
2025-08-17 18:37:54 +0800

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,5 @@
-
 #!/usr/bin/env bash
-set -eux
-
-# 顯示 Python 版本
-python -V
-
-# 安裝依賴
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# 安裝 Playwright 及 Chromium 瀏覽器
+set -e
+pip install --no-cache-dir -r requirements.txt
+# 安裝 Playwright 及其相依（Chromium）
 playwright install --with-deps chromium
